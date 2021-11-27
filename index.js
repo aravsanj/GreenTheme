9d5cc7e25a6b39d6961a49d14c1a53cfa99d3dcd
aravsanj <saravind436@gmail.com>
2021-11-27 14:25:08 +0530

    starter

--- index.js
+++
@@ -1,8 +0,0 @@
-const toggleButton = document.getElementById("toggle-button")
-const navbarLinks = document.getElementsByClassName("navbar-links-container")[0]
-
-toggleButton.addEventListener("click", () => toggle())
-
-function toggle() {
-  navbarLinks.classList.toggle("active")
-}
